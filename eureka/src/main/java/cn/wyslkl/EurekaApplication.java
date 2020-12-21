8a1cdc05bab9cd00549c38345c6d743fd2e1e81d
wysss <1243876958@qq.com>
2020-12-22 01:51:45 +0800

    优化启动日志显示

---
+++ eureka/src/main/java/cn/wyslkl/EurekaApplication.java
@@ -0,0 +1,29 @@
+package cn.wyslkl;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.core.env.Environment;
+
+import static org.slf4j.LoggerFactory.*;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+
+    private static final Logger LOG= LoggerFactory.getLogger(EurekaApplication.class);
+
+    /*public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+*/
+    public static void main(String[] args){
+        SpringApplication app=new SpringApplication(EurekaApplication.class);
+        Environment env=app.run(args).getEnvironment();
+        LOG.info("启动成功！");
+        LOG.info("eureka地址: \thttp://localhost:7777");
+
+    }
+}
