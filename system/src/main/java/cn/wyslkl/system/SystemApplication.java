b2cd24d8cb4c90edd5aa3d0c2f2eaf6f1f987657
wysss <1243876958@qq.com>
2020-12-22 03:10:33 +0800

    增加system子项目

--- system/src/main/java/cn/wyslkl/system/SystemApplication.java
+++ system/src/main/java/cn/wyslkl/system/SystemApplication.java
@@ -4,9 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.core.env.Environment;
 
-
+@EnableEurekaClient
 @SpringBootApplication
 public class SystemApplication {
 
