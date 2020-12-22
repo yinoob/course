b8950c57c3c3749847caef92e60ed46e7729b7d0
wysss <1243876958@qq.com>
2020-12-22 20:42:44 +0800

    新建公共模块server

---
+++ server/src/main/java/cn/wyslkl/server/mapper/TestMapper.java
@@ -0,0 +1,9 @@
+package cn.wyslkl.server.mapper;
+
+import cn.wyslkl.server.domain.Test;
+
+import java.util.List;
+
+public interface TestMapper {
+     List<Test> list();
+}
---
+++ server/src/main/java/cn/wyslkl/server/service/TestService.java
@@ -0,0 +1,19 @@
+package cn.wyslkl.server.service;
+
+import cn.wyslkl.server.domain.Test;
+import cn.wyslkl.server.mapper.TestMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class TestService {
+
+    @Resource
+    private TestMapper testMapper;
+
+    public List<Test> list(){
+        return testMapper.list();
+    }
+}
