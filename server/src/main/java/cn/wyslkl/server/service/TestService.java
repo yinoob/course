947ce8e150b2585122f5675b5c781c8a5f27a8e9
wysss <1243876958@qq.com>
2020-12-23 02:54:33 +0800

    example使用示例，使用example可以快速完成单表的增删改查

--- server/src/main/java/cn/wyslkl/server/service/TestService.java
+++ server/src/main/java/cn/wyslkl/server/service/TestService.java
@@ -1,6 +1,7 @@
 package cn.wyslkl.server.service;
 
 import cn.wyslkl.server.domain.Test;
+import cn.wyslkl.server.domain.TestExample;
 import cn.wyslkl.server.mapper.TestMapper;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +15,8 @@ public class TestService {
     private TestMapper testMapper;
 
     public List<Test> list(){
-        return testMapper.list();
+        //TestExample testExample = new TestExample();
+        //testExample.setOrderByClause("id asc");
+        return testMapper.selectByExample(null);
     }
 }
--- system/src/main/java/cn/wyslkl/system/config/SystemApplication.java
+++ system/src/main/java/cn/wyslkl/system/config/SystemApplication.java
@@ -6,11 +6,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
 @EnableEurekaClient
 @SpringBootApplication
-@MapperScan("cn.wyslkl.system.mapper")
+@ComponentScan("cn.wyslkl")
+@MapperScan("cn.wyslkl.server.mapper")
 public class SystemApplication {
 
     private static final Logger LOG= LoggerFactory.getLogger(SystemApplication.class);
