5ac2bfad38210a84886a9a50b192f30f978ad54e
wysss <1243876958@qq.com>
2020-12-22 22:15:54 +0800

    集成mybatis-generator

--- server/src/main/java/cn/wyslkl/server/domain/Test.java
+++ server/src/main/java/cn/wyslkl/server/domain/Test.java
@@ -21,5 +21,15 @@ public class Test {
         this.name = name;
     }
 
-
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", name=").append(name);
+        sb.append("]");
+        return sb.toString();
+    }
 }
--- server/src/main/java/cn/wyslkl/server/mapper/TestMapper.java
+++ server/src/main/java/cn/wyslkl/server/mapper/TestMapper.java
@@ -1,9 +1,30 @@
 package cn.wyslkl.server.mapper;
 
 import cn.wyslkl.server.domain.Test;
-
+import cn.wyslkl.server.domain.TestExample;
 import java.util.List;
+import org.apache.ibatis.annotations.Param;
 
 public interface TestMapper {
-     List<Test> list();
+    long countByExample(TestExample example);
+
+    int deleteByExample(TestExample example);
+
+    int deleteByPrimaryKey(String id);
+
+    int insert(Test record);
+
+    int insertSelective(Test record);
+
+    List<Test> selectByExample(TestExample example);
+
+    Test selectByPrimaryKey(String id);
+
+    int updateByExampleSelective(@Param("record") Test record, @Param("example") TestExample example);
+
+    int updateByExample(@Param("record") Test record, @Param("example") TestExample example);
+
+    int updateByPrimaryKeySelective(Test record);
+
+    int updateByPrimaryKey(Test record);
 }
