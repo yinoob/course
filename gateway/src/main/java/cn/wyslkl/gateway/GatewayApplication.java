5e2032c958eecc1c85ba91c7bc9b05fe87897eb4
wysss <1243876958@qq.com>
2020-12-24 14:36:40 +0800

    使用lb://+注册中心名称作路由转发

--- gateway/src/main/java/cn/wyslkl/gateway/GatewayApplication.java
+++ gateway/src/main/java/cn/wyslkl/gateway/GatewayApplication.java
@@ -5,7 +5,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.util.pattern.PathPatternParser;
 
 @EnableEurekaClient
 @SpringBootApplication
@@ -13,11 +19,11 @@ public class GatewayApplication {
 
     private static final Logger LOG= LoggerFactory.getLogger(GatewayApplication.class);
 
-    public static void main(String[] args){
-        SpringApplication app=new SpringApplication(GatewayApplication.class);
-        Environment env=app.run(args).getEnvironment();
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(GatewayApplication.class);
+        Environment env = app.run(args).getEnvironment();
         LOG.info("启动成功！");
         LOG.info("eureka地址: \thttp://localhost:8999");
-
     }
+
 }
