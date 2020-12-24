22cc1464feeb68cdff6883c89ea21e293539b816
wysss <1243876958@qq.com>
2020-12-25 05:28:03 +0800

    增加刷新功能

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -1,4 +1,11 @@
 <template>
+    <div>
+        <p>
+    <button v-on:click="list" class="btn btn-white btn-default btn-round">
+        <i class="ace-icon fa fa-refresh"></i>
+        刷新
+    </button>
+        </p>
     <table id="simple-table" class="table  table-bordered table-hover">
         <thead>
         <tr>
@@ -74,6 +81,7 @@
 
         </tbody>
     </table>
+    </div>
 </template>
 <script>
     export default {
