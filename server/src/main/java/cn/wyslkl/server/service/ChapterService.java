a8eb94bf10e806dcf0fdfcfde058852baa0ac909
wysss <1243876958@qq.com>
2020-12-24 23:32:12 +0800

    集成分页插件PageHelper

--- server/src/main/java/cn/wyslkl/server/service/ChapterService.java
+++ server/src/main/java/cn/wyslkl/server/service/ChapterService.java
@@ -4,6 +4,7 @@ import cn.wyslkl.server.domain.Chapter;
 import cn.wyslkl.server.domain.ChapterExample;
 import cn.wyslkl.server.dto.ChapterDto;
 import cn.wyslkl.server.mapper.ChapterMapper;
+import com.github.pagehelper.PageHelper;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +18,7 @@ public class ChapterService {
     private ChapterMapper chapterMapper;
 
     public List<ChapterDto> list(){
+        PageHelper.startPage(1,1);
         ChapterExample chapterExample = new ChapterExample();
         List<Chapter> chapterList = chapterMapper.selectByExample(chapterExample);
         List<ChapterDto> chapterDtoList=new ArrayList<ChapterDto>();
