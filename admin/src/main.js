08e7466b71c07491138f92c09bfac5c38139df78
wysss <1243876958@qq.com>
2020-12-27 02:56:42 +0800

    增加generator模块，用于代码生成 集成freemarker

--- admin/src/main.js
+++ admin/src/main.js
@@ -20,6 +20,24 @@ Vue.prototype.$ajax=axios
 
 Vue.config.productionTip = false
 
+/**
+ * axios拦截器
+
+axios.interceptors.request.use(function (config) {
+  console.log("请求：", config);
+  let token = Tool.getLoginUser().token;
+  if (Tool.isNotEmpty(token)) {
+    config.headers.token = token;
+    console.log("请求headers增加token:", token);
+  }
+  return config;
+}, error => {});
+axios.interceptors.response.use(function (response) {
+  console.log("返回结果：", response);
+  return response;
+}, error => {});
+*/
+
 new Vue({
   router,
   render: h => h(App),
---
+++ generator/src/main/java/cn/wyslkl/generator/test/Test.java
@@ -0,0 +1,8 @@
+package cn.wyslkl.generator.test;
+
+public class Test {
+    /**
+    * ID111
+    */
+    private String id;
+}
