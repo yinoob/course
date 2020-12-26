76ec0ae70afddae7faff316dd9b479786dd88b6a
wysss <1243876958@qq.com>
2020-12-26 14:44:50 +0800

    增加了校验工具类VaildatorUtil

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -146,6 +146,7 @@
 
                 // 保存校验
                 if (!Validator.require(_this.chapter.name, "名称")
+                        || !Validator.require(_this.chapter.courseId, "课程ID")
                     || !Validator.length(_this.chapter.courseId, "课程ID", 1, 8)) {
                     return;
                 }
