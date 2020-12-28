2886332dc5acd48a750826473ba3930f5ca21b6d
wysss <1243876958@qq.com>
2020-12-29 00:17:11 +0800

    讲师表设计和基本代码生成

---
+++ business/src/main/java/cn/wyslkl/business/controller/admin/TeacherController.java
@@ -0,0 +1,69 @@
+package cn.wyslkl.business.controller.admin;
+
+import cn.wyslkl.server.dto.TeacherDto;
+import cn.wyslkl.server.dto.PageDto;
+import cn.wyslkl.server.dto.ResponseDto;
+import cn.wyslkl.server.service.TeacherService;
+import cn.wyslkl.server.util.ValidatorUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.context.annotation.ComponentScan;
+import org.mybatis.spring.annotation.MapperScan;
+
+import javax.annotation.Resource;
+
+@RestController
+@ComponentScan("cn.wyslkl")
+@MapperScan("cn.wyslkl.server.mapper")
+@ResponseBody
+@CrossOrigin
+@RequestMapping("/admin/teacher")
+public class TeacherController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(TeacherController.class);
+    public static final String BUSINESS_NAME = "讲师";
+
+    @Resource
+    private TeacherService teacherService;
+
+    /**
+     * 列表查询
+     */
+    @PostMapping("/list")
+    public ResponseDto list( PageDto pageDto) {
+        ResponseDto responseDto = new ResponseDto();
+        teacherService.list(pageDto);
+        responseDto.setContent(pageDto);
+        return responseDto;
+    }
+
+    /**
+     * 保存，id有值时更新，无值时新增
+     */
+    @PostMapping("/save")
+    public ResponseDto save( TeacherDto teacherDto) {
+        // 保存校验
+        ValidatorUtil.require(teacherDto.getName(), "姓名");
+        ValidatorUtil.length(teacherDto.getName(), "姓名", 1, 50);
+        ValidatorUtil.length(teacherDto.getNickname(), "昵称", 1, 50);
+        ValidatorUtil.length(teacherDto.getImage(), "头像", 1, 100);
+        ValidatorUtil.length(teacherDto.getPosition(), "职位", 1, 50);
+        ValidatorUtil.length(teacherDto.getMotto(), "座右铭", 1, 50);
+        ValidatorUtil.length(teacherDto.getIntro(), "简介", 1, 500);
+        ResponseDto responseDto = new ResponseDto();
+        teacherService.save(teacherDto);
+        responseDto.setContent(teacherDto);
+        return responseDto;
+    }
+
+    /**
+     * 删除
+     */
+    @PostMapping("/delete")
+    public ResponseDto delete( String id) {
+        ResponseDto responseDto = new ResponseDto();
+        teacherService.delete(id);
+        return responseDto;
+    }
+}
