c559ecf2c0f904400c26dab7a0ee98f3aaefcc8c
wysss <1243876958@qq.com>
2021-01-03 03:52:37 +0800

    网站首页开发,集成album模板

---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer class="text-muted">
+    <div class="container">
+      <p class="float-right">
+        <a href="#">回到顶部</a>
+      </p>
+      <p>欢迎使用松哥在线课堂，程序员的梦工厂，致力于全栈技术推广</p>
+      <p>2021 @wys</p>
+    </div>
+  </footer>
+</template>
+
+<script>
+
+  export default {
+    name: 'theFooter',
+  }
+</script>
